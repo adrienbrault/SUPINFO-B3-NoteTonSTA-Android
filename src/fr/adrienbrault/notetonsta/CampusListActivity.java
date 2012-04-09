c695106f14f42d1cb2353b4cc8ffc9a5eabd9846
Adrien BRAULT <monsti@gmail.com>
2012-04-09 17:09:03 +0200

    Initial commit

---
+++ src/fr/adrienbrault/notetonsta/CampusListActivity.java
@@ -0,0 +1,15 @@
+package fr.adrienbrault.notetonsta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Author: Adrien Brault
+ * Date: 09/04/12
+ * Time: 16:54
+ */
+public class CampusListActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/fr/adrienbrault/notetonsta/EvaluateInterventionActivity.java
@@ -0,0 +1,15 @@
+package fr.adrienbrault.notetonsta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Author: Adrien Brault
+ * Date: 09/04/12
+ * Time: 16:55
+ */
+public class EvaluateInterventionActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
